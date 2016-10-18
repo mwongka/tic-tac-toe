f0b78d473e8532cdb4e08b677e7c74f72acb34a9
mwongka <mikekawong@gmail.com>
2016-10-18 12:17:32 -0700

    implement vertical check logic to see if current winner has won the game

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 //need game class
 // player will be 'x' or 'o'
 var Board = function(player1, player2){
-  this.board = [[0,0,0],
+  this.board = [[0,'X','X'],
                 [0,0,0],
                 [0,0,0]];
   this.player1 = player1;
@@ -22,18 +22,48 @@ Board.prototype.playerMove = function(row, col){
     console.log('spot taken, try an empty spot') 
   } else {
     this.board[row][col] = this.currentPlayer;
-    this.checkWin();
-    console.log(this.showBoard());
-    this.currentPlayer = this.currentPlayer === this.player1 ? this.player2 : this.player1;
-    console.log('Player ' + this.currentPlayer + '\'s move next!');
+    if(this.checkWin(this.currentPlayer, row, col)){
+      console.log('Player ' + this.currentPlayer + ' is the winner!');
+    } else {
+      console.log(this.showBoard());
+      this.currentPlayer = this.currentPlayer === this.player1 ? this.player2 : this.player1;
+      console.log('Player ' + this.currentPlayer + '\'s move next!'); 
+    }
   }
 }
 
-Board.prototype.checkWin = function(){
-
+Board.prototype.checkWin = function(player, row, col){
+  //can brute force this an iterate through to check if player has won
+  if(this.checkHorizontal(player, row, col)){
+    return true;
+  } else if (this.checkVertical(player, row, col)){
+    return true;
+  } else if (this.checkDiagonal(player, row, col)){
+    return true;
+  } else {
+    return false;
+  }
 }
 
-var game = new Board('x', 'o');
+Board.prototype.checkHorizontal = function(player, row, col){
+  var winnerFound = true;
+  this.board[row].forEach(function(piece){
+    if(piece !== player){
+      winnerFound = false;
+      return winnerFound;
+    }
+  });
+  return winnerFound
+}
 
-console.log(game.playerMove(0,0));
-console.log(game.playerMove(0,0));
+Board.prototype.checkVertical = function(player, row, col){
+  
+}
+
+Board.prototype.checkDiagonal = function(player, row, col){
+  
+}
+
+var game = new Board('X', 'O');
+
+console.log(game.playerMove(1,0));
