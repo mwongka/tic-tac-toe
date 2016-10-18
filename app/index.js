9c2132a37e10e907d39b490e001e0deabf1ea0b8
mwongka <mikekawong@gmail.com>
2016-10-18 11:49:57 -0700

    update board organization in console

--- app/index.js
+++ app/index.js
@@ -6,10 +6,15 @@ var Board = function(player1, player2){
                 [0,0,0]];
   this.player1 = player1;
   this.player2 = player2;
+  this.turn = 1;
 }
 
 Board.prototype.showBoard = function() {
-  return this.board.split(',');
+  var consoleBoard = ''
+  this.board.forEach(function(curr,i){
+    consoleBoard += curr.toString() + '\n';
+  })
+  return consoleBoard;
 }
 
 var game = new Board('x', 'o');
